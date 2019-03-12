005771569f2179a2cd7236a3230bc73687bcf244
Mitu Rostislav <rmitu@tacitknowledge.com>
2019-03-12 18:01:26 +0200

    Clone the existing frame if there is one rather than creating a new one

--- sliderFrameUtils.js
+++ sliderFrameUtils.js
@@ -1,16 +1,23 @@
-export function createFrame({ nrOfTotalSlideElements, visibleSlides, slides }) {
-  const frame = document.createElement('div');
-  frame.classList.add('frame');
-  frame.style.setProperty(
+export function createFrame({
+  nrOfTotalSlideElements,
+  visibleSlides,
+  slides,
+  frame,
+}) {
+  const newFrame = frame
+    ? frame.cloneNode(false)
+    : document.createElement('div');
+  newFrame.classList.add('frame');
+  newFrame.style.setProperty(
     'width',
     `${(100 * nrOfTotalSlideElements) / visibleSlides}%`
   );
-  frame.style.setProperty('display', 'flex');
+  newFrame.style.setProperty('display', 'flex');
   slides.forEach(slide => {
     slide.style.setProperty('width', `${100 / nrOfTotalSlideElements}%`);
-    frame.appendChild(slide);
+    newFrame.appendChild(slide);
   });
-  return frame;
+  return newFrame;
 }
 
 export const onSliderFrameInit = onInit => () => {
